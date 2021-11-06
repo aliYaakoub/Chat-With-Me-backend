34176800790dbe53de06dd68a37b966607473af6
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-11-06 22:54:03 +0200

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(`mongodb://localhost/chatWithMe`);
+// mongoose.connect(`mongodb://localhost/chatWithMe`);
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', err => console.error(err));
 db.once('open', () => console.log('connected to MongoDB'))
